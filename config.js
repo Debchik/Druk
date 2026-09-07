42925569e843bdd434968c5773a361b2d7d2d259
Daniil Pavshinskiy <94741854+Debchik@users.noreply.github.com>
2026-09-07 23:33:09 +0300

    Connect beta Google Form

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Вставьте публичную ссылку на вашу Google-форму между кавычками.
 // Пример: https://docs.google.com/forms/d/e/ВАШ_ID/viewform
 window.DRUK_CONFIG = {
-  betaFormUrl: ""
+  betaFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSfqLryYMuuPwr9OA9haR12AP2a9Lz95MpGKLvumbyxjDjsKww/viewform?usp=header"
 };
