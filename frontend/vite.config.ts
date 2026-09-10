e94d4d152c579e2022182239263b176eaef89175
Daniil Pavshinskiy <94741854+Debchik@users.noreply.github.com>
2026-09-10 13:16:50 +0300

    Fix Vite config typecheck

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, '.', '')
   return { plugins: [react()], base: env.VITE_BASE_PATH || '/' }
 })
