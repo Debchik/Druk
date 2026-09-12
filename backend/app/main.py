f4d7e33b18756f3ec548b3348b34026e1e3e5711
Daniil Pavshinskiy <94741854+Debchik@users.noreply.github.com>
2026-09-12 19:25:45 +0300

    Wire bug report API

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,5 +2,7 @@
 
 from .core import app, fetch_github_issues, httpx, settings
 from .extended import router
+from .bug_reports import router as bug_reports_router
 
 app.include_router(router)
+app.include_router(bug_reports_router)
